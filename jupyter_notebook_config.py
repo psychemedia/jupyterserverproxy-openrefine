40152e8640885d1eb8fbd4fa7f01b70ffe71d1f7
Tony Hirst <tony.hirst@gmail.com>
2019-02-08 23:05:30 +0000

    Messed up paths dues to version number

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -1,12 +1,16 @@
 # Traitlet configuration file for jupyter-notebook.
-
+#Can we guarantee access to environment vars in this file?
+#The start command is tied to the OpenRefine version number
+#unless we rename it earlier in the setup process
 c.ServerProxy.servers = {
     'openrefine': {
-        'command': ['/home/jovyan/.openrefine/openrefine-2.8/refine', '-p', '{port}','-d','/home/jovyan/openrefine'],
+        'command': ['/home/jovyan/.openrefine/openrefine-3.1/refine','-ip','0.0.0.0', '-p', '{port}','-d','/home/jovyan/openrefine'],
         'port': 3333,
         'timeout': 120,
         'launcher_entry': {
-            'title': 'OpenRefine'
+            'enabled': True,
+            'icon_path': '/home/jovyan/.jupyter/open-refine-logo.svg',
+            'title': 'OpenRefine',
         },
     },
 }
