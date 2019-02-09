f42f18faaa8577ad0c75e5547eb64b1fb5ff37fd
Tony Hirst <tony.hirst@gmail.com>
2019-02-09 08:03:19 +0000

    Does setting IP addresses to 0.0.0.0 help?

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -2,7 +2,7 @@
 
 c.ServerProxy.servers = {
     'openrefine': {
-        'command': ['/home/jovyan/.openrefine/openrefine-2.8/refine', '-p', '{port}','-d','/home/jovyan/openrefine'],
+        'command': ['/home/jovyan/.openrefine/openrefine-2.8/refine', '-i', '0.0.0.0', '-p', '{port}','-d','/home/jovyan/openrefine'],
         'port': 3333,
         'timeout': 120,
         'launcher_entry': {
